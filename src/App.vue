9193203932b44cc88541b6f9d5104a823c539f32
zikai <zikai33@yahoo.com>
2022-08-02 00:49:43 -0400

    update nav demo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <router-view/>
+    <Nav />
+    <router-view />
 </template>
+
+<script>
+import Nav from "./components/Nav.vue";
+export default {
+    name: "App",
+    components: {
+        Nav,
+    },
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+        <router-link to="/"><a>Home</a></router-link>
+        <router-link to="/about"><a>About</a></router-link>
+        <router-link to="/education"><a>Education</a></router-link>
+        <router-link to="/game"><a>Game</a></router-link>
+        <router-link to="/resume"><a>Resume</a></router-link>
+        <router-link to="/contact"><a>Contact</a></router-link>
+    </nav>
+</template>
+
+<style scoped>
+nav {
+    width: 100%;
+}
+</style>
