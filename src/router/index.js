eaff0fc3a8371f90ccc35d1769802620a50ab95d
zikai <zikai33@yahoo.com>
2022-08-02 00:39:37 -0400

    Project enviroment setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Education from '../views/Education.vue';
+import Resume from '../views/Resume.vue';
+import Contact from '../views/Contact.vue';
+import Game from '../views/Game.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: About
+  },
+  {
+    path: '/education',
+    name: 'education',
+    component: Education
+  },
+  {
+    path: '/resume',
+    name: 'resume',
+    component: Resume
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  },
+  {
+    path: '/game',
+    name: 'game',
+    component: Game
   }
 ]
 
